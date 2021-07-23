642bccd72f7552499621582ee5006a210eb674ba
sharron <10656035@ntub.edu.tw>
2021-07-23 12:08:53 +0800

    Add notes

---
+++ lib/notes.dart
@@ -0,0 +1,56 @@
+// import 'package:My_Day_app/main.dart';
+import 'package:flutter/material.dart';
+
+import 'learn.dart';
+import 'main.dart';
+import 'notes_add.dart';
+
+class NotesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/home': (BuildContext context) => new HomePageWidget(),
+        
+        '/learn' : (BuildContext context) => new LearnPage(),
+      },
+      home: Scaffold(
+        body: NotesPageWidget(),
+      ),
+    );
+  }
+}
+
+enum WhyFarther { harder, smarter, selfStarter, tradingCharter }
+
+class NotesPageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffF86D67),
+        title:Text('筆記',style: TextStyle(fontSize: 20)),
+        leading:IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: () {
+            Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LearnPage()));
+          },
+        ), 
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => NotesAddPage()));
+            },
+          ),
+        ],
+      ),  
+    );
+  }
+}
+
+
+
