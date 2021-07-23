0a15daf8c7bdb4d73dfad69c50f3dda17d0d1db5
sharron <10656035@ntub.edu.tw>
2021-07-23 12:05:39 +0800

    Add bestfriends

---
+++ lib/bestfriend.dart
@@ -0,0 +1,56 @@
+// import 'package:My_Day_app/main.dart';
+import 'package:flutter/material.dart';
+
+import 'learn.dart';
+import 'main.dart';
+import 'notes_add.dart';
+import 'friends.dart';
+class BestFriendPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/home': (BuildContext context) => new HomePageWidget(),
+        
+        '/learn' : (BuildContext context) => new LearnPage(),
+      },
+      home: Scaffold(
+        body: BestFriendPageWidget(),
+      ),
+    );
+  }
+}
+
+enum WhyFarther { harder, smarter, selfStarter, tradingCharter }
+
+class BestFriendPageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffF86D67),
+        title:Text('摯友',style: TextStyle(fontSize: 20)),
+        leading:IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: () {
+            Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => FriendsPage()));
+          },
+        ), 
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => NotesAddPage()));
+            },
+          ),
+        ],
+      ),  
+    );
+  }
+}
+
+
+
