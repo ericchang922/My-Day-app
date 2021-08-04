f4c32aac4b84c062ecf5ebe9d6eb95a6808d6883
ChangCheWei <10656004@ntub.edu.tw>
2021-08-05 02:07:55 +0800

    Update set language

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,35 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'home.dart';
-import 'group/group_list_page.dart';
-import 'learn.dart';
 import 'login.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'My Day',
+      localizationsDelegates: [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [const Locale('zh','TW')],
       theme: ThemeData(
-        primaryColor: Color(0xffF86D67),  //主色
-        primaryColorLight: Color(0xffFFAAA6),  //次色
+        primaryColor: Color(0xffF86D67), //主色
+        primaryColorLight: Color(0xffFFAAA6), //次色
         primaryColorDark: Color(0xffFFF5F5), //淡色
-        accentColor: Color(0xffF86D67), 
+        accentColor: Color(0xffF86D67),
+        bottomAppBarColor: Color(0xffFB8B86),
       ),
       debugShowCheckedModeBanner: false,
       home: Home(),
