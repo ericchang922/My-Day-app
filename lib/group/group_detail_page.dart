5018b7b4fe7801f28a60376ccf4468946e1e3bad
chehui0512 <10656009@ntub.edu.tw>
2021-08-06 02:07:11 +0800

    Create group log

--- lib/group/group_detail_page.dart
+++ lib/group/group_detail_page.dart
@@ -5,7 +5,9 @@ import 'package:My_Day_app/common_note/common_note_list_page.dart';
 import 'package:My_Day_app/common_schedule/common_schedule_list_page.dart';
 import 'package:My_Day_app/common_studyplan/common_studyplan_list_page.dart';
 import 'package:My_Day_app/models/get_group_model.dart';
+import 'package:My_Day_app/models/group_log_model.dart';
 import 'package:My_Day_app/vote/vote_page.dart';
+import 'package:date_format/date_format.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
@@ -26,21 +28,27 @@ class GroupDetailPage extends StatefulWidget {
 
 class _GroupDetailWidget extends State<GroupDetailPage> {
   GetGroupModel _getGroupModel = null;
-  List<Widget> votesList = [];
+  GroupLogModel _groupLogModel = null;
+
+  List<Widget> _votesList = [];
+  List _groupLogDate = [];
+  List _groupLogDateIsShow = [];
 
   int groupNum;
   String uid = 'lili123';
+
   _GroupDetailWidget(this.groupNum);
 
   @override
   void initState() {
+    super.initState();
     print(groupNum);
     _getGroupRequest();
-    super.initState();
+    _groupLogRequest();
   }
 
   void _getGroupRequest() async {
-    // var reponse = await rootBundle.loadString('assets/json/get_group.json');
+    // var jsonString = await rootBundle.loadString('assets/json/get_group.json');
 
     var httpClient = HttpClient();
     var request = await httpClient.getUrl(Uri.http('myday.sytes.net',
@@ -59,11 +67,46 @@ class _GroupDetailWidget extends State<GroupDetailPage> {
     });
   }
 
+  void _groupLogRequest() async {
+    // var jsonString = await rootBundle.loadString('assets/json/group_log.json');
+
+    var httpClient = HttpClient();
+    var request = await httpClient.getUrl(Uri.http('myday.sytes.net',
+        '/group/get_log/', {'uid': uid, 'groupNum': groupNum.toString()}));
+    var response = await request.close();
+    var jsonString = await response.transform(utf8.decoder).join();
+    httpClient.close();
+    print(jsonString);
+
+    var jsonBody = json.decode(jsonString);
+
+    var groupLogModel = GroupLogModel.fromJson(jsonBody);
+
+    setState(() {
+      _groupLogModel = groupLogModel;
+      _groupLogDate = new List();
+      _groupLogDateIsShow = new List();
+
+      for (int i = 0; i < _groupLogModel.groupContent.length; i++) {
+        DateTime doTime = _groupLogModel.groupContent[i].doTime;
+        String dateString = formatDate(
+            DateTime(doTime.year, doTime.month, doTime.day),
+            [yyyy, '年', mm, '月', dd, '日 ']);
+        _groupLogDateIsShow.add(false);
+        if (_groupLogDate.indexOf(dateString) == -1) {
+          _groupLogDate.add(dateString);
+          _groupLogDateIsShow[i] = true;
+        }
+      }
+    });
+    print(_groupLogDate);
+  }
+
   selectedItem(BuildContext context, item) async {
     switch (item) {
       case 0:
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => GroupInvitePage()));
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => GroupInvitePage(groupNum)));
         break;
       case 1:
         Navigator.of(context).push(
@@ -82,10 +125,10 @@ class _GroupDetailWidget extends State<GroupDetailPage> {
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-    if (_getGroupModel != null) {
+    if (_getGroupModel != null && _groupLogModel != null) {
       return Scaffold(
           appBar: AppBar(
-            backgroundColor: Theme.of(context).primaryColor,
+              backgroundColor: Theme.of(context).primaryColor,
               actions: [
                 PopupMenuButton<int>(
                   offset: Offset(50, 50),
@@ -145,7 +188,7 @@ class _GroupDetailWidget extends State<GroupDetailPage> {
               ),
               title: Text(_getGroupModel.title,
                   style: TextStyle(fontSize: screenSize.width * 0.052))),
-          body: _buildGroup(context));
+          body: Container(color: Colors.white, child: _buildGroup(context)));
     } else {
       return Center(child: CircularProgressIndicator());
     }
@@ -153,19 +196,29 @@ class _GroupDetailWidget extends State<GroupDetailPage> {
 
   Widget _buildGroup(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Column(
-          children: [
-            SizedBox(
-                height: screenSize.height * 0.50,
-                child: _buildGroupState(context)),
-            Expanded(child: _buildGroupList(context))
-          ],
-        ),
-        if (_getGroupModel.vote.length != 0) _buildVote(context),
-      ],
-    );
+    if (_getGroupModel.vote.length != 0) {
+      return Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                  child: Container(
+                      margin: EdgeInsets.only(top: screenSize.height * 0.067),
+                      child: _buildGroupState(context))),
+              _buildGroupList(context)
+            ],
+          ),
+          _buildVote(context),
+        ],
+      );
+    } else {
+      return Column(
+        children: [
+          Expanded(child: _buildGroupState(context)),
+          _buildGroupList(context)
+        ],
+      );
+    }
   }
 
   Widget _buildVoteState(bool isVoteType, int voteNum) {
@@ -179,25 +232,25 @@ class _GroupDetailWidget extends State<GroupDetailPage> {
                   fontSize: screenSize.width * 0.041,
                   color: Theme.of(context).primaryColor)),
           onTap: () {
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => VotePage(voteNum)));
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => VotePage(voteNum, groupNum)));
           },
         ),
       );
-    }
-    if (isVoteType == false)
+    } else {
       return Text('已投票',
           style: TextStyle(
               fontSize: screenSize.width * 0.041, color: Color(0xff959595)));
+    }
   }
 
   Widget _buildVote(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
     setState(() {
-      votesList = [];
+      _votesList = [];
       for (int i = 0; i < _getGroupModel.vote.length; i++) {
         var votes = _getGroupModel.vote[i];
-        votesList.add(
+        _votesList.add(
           ListTile(
               contentPadding: EdgeInsets.symmetric(
                   horizontal: screenSize.width * 0.05, vertical: 0.0),
@@ -245,7 +298,7 @@ class _GroupDetailWidget extends State<GroupDetailPage> {
                   ),
                 ),
                 backgroundColor: Theme.of(context).primaryColorDark,
-                children: votesList),
+                children: _votesList),
           ),
         ),
       ],
@@ -253,14 +306,67 @@ class _GroupDetailWidget extends State<GroupDetailPage> {
   }
 
   Widget _buildGroupState(BuildContext context) {
-    return ListView.builder(
-      itemCount: 30,
-      itemBuilder: (content, index) {
-        return ListTile(
-          title: Text('${index}'),
-        );
-      },
-    );
+    var screenSize = MediaQuery.of(context).size;
+    if (_groupLogModel.groupContent.length != 0) {
+      return ListView.builder(
+        itemCount: _groupLogModel.groupContent.length,
+        itemBuilder: (content, index) {
+          var groupContent = _groupLogModel.groupContent[index];
+          DateTime doTime = groupContent.doTime;
+          String dateString = formatDate(
+              DateTime(doTime.year, doTime.month, doTime.day),
+              [yyyy, '年', mm, '月', dd, '日 ']);
+          String timeString = formatDate(
+              DateTime(doTime.year, doTime.month, doTime.day, doTime.hour,
+                  doTime.minute),
+              [HH, ':', nn]);
+          return Container(
+            margin: EdgeInsets.only(top: screenSize.height * 0.01),
+            child: Column(
+              children: [
+                Visibility(
+                    visible: _groupLogDateIsShow[index],
+                    child: Text(
+                      dateString,
+                      style: TextStyle(
+                        fontSize: screenSize.width * 0.033,
+                      ),
+                    )),
+                Container(
+                  margin: EdgeInsets.only(
+                      top: screenSize.height * 0.01,
+                      bottom: screenSize.height * 0.005,
+                      left: screenSize.height * 0.01),
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Text(
+                        timeString,
+                        style: TextStyle(
+                          fontSize: screenSize.width * 0.033,
+                        ),
+                      ),
+                      Container(
+                          margin:
+                              EdgeInsets.only(left: screenSize.height * 0.02),
+                          child: Text(
+                            groupContent.name + " " + groupContent.logContent,
+                            style: TextStyle(
+                              fontSize: screenSize.width * 0.033,
+                            ),
+                          ))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      );
+    } else {
+      return Container(
+          alignment: Alignment.center, height: 100, child: Text('目前沒有任何log喔！'));
+    }
   }
 
   Widget _buildGroupList(BuildContext context) {
