a2d910e5715a03388bc6101c2f2e6e4fb28eb7c6
BotoWu <10656003@ntub.edu.tw>
2021-09-09 23:46:59 +0800

    Create timetable create

---
+++ lib/timetable/timetable_create.dart
@@ -0,0 +1,27 @@
+import 'package:My_Day_app/timetable/timetable_create_popup_menu.dart';
+import 'package:flutter/material.dart';
+
+const PrimaryColor = const Color(0xFFF86D67);
+
+class TimetableCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TimetableCreate();
+  }
+}
+
+class TimetableCreate extends State {
+  get child => null;
+  get left => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffF86D67),
+        title: Text('110年　第一學期', style: TextStyle(fontSize: 20)),
+        actions: [createPopMenu(context)],
+      ),
+    );
+  }
+}
