677409d66de41816b5efe5d7a33c36b837447596
BotoWu <10656003@ntub.edu.tw>
2021-09-20 23:54:48 +0800

    Update timetable_create

--- lib/timetable/timetable_create.dart
+++ lib/timetable/timetable_create.dart
@@ -1,3 +1,5 @@
+import 'package:My_Day_app/timetable/timetable_action_list.dart';
+import 'package:My_Day_app/timetable/timetable_create_cancel.dart';
 import 'package:My_Day_app/timetable/timetable_create_popup_menu.dart';
 import 'package:flutter/material.dart';
 
@@ -21,6 +23,61 @@ class TimetableCreate extends State {
         backgroundColor: Color(0xffF86D67),
         title: Text('110年　第一學期', style: TextStyle(fontSize: 20)),
         actions: [createPopMenu(context)],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(42, 106, 0, 0),
+            child: Column(
+              children: [
+                Row(
+                  children: [],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        color: Theme.of(context).bottomAppBarColor,
+        child: SafeArea(
+          top: false,
+          child: BottomAppBar(
+            elevation: 0,
+            child: Row(children: <Widget>[
+              Expanded(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  child: RawMaterialButton(
+                    elevation: 0,
+                    child: Image.asset(
+                      'assets/images/cancel.png',
+                      width: MediaQuery.of(context).size.width * 0.05,
+                    ),
+                    fillColor: Theme.of(context).primaryColorLight,
+                    onPressed: () async => await timetableCreateCancel(context)
+                  ),
+                ),
+              ), // 取消按鈕
+              Expanded(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  child: RawMaterialButton(
+                      elevation: 0,
+                      child: Image.asset(
+                        'assets/images/confirm.png',
+                        width: MediaQuery.of(context).size.width * 0.05,
+                      ),
+                      fillColor: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pop();
+                      }),
+                ),
+              )
+            ]),
+          ),
+        ),
       ),
     );
   }
