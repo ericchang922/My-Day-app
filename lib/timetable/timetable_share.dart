2848c76076dc32afa4f2e2c1ec3c0a069811c03f
chehui0512 <10656009@ntub.edu.tw>
2021-10-05 19:51:46 +0800

    Update timetable_share margin

--- lib/timetable/timetable_share.dart
+++ lib/timetable/timetable_share.dart
@@ -39,8 +39,8 @@ Future<bool> timetableShare(BuildContext context) async {
                 ),
                 SizedBox(height: _height * 0.02),
                 Container(
-                  margin: EdgeInsets.only(
-                      left: _width * 0.15, right: _width * 0.15),
+                  margin:
+                      EdgeInsets.only(left: _width * 0.1, right: _width * 0.1),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -68,19 +68,20 @@ Future<bool> timetableShare(BuildContext context) async {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Container(
-                      width: _width * 0.5,
-                      decoration: new BoxDecoration(
-                        color: Color(0xffF8F8F8),
-                        borderRadius:
-                            BorderRadius.all(Radius.circular(_height * 0.01)),
-                      ),
-                      margin: EdgeInsets.only(left: _width * 0.05),
-                      child: Padding(
-                        padding: EdgeInsets.all(_height * 0.01),
-                        child: Text(
-                          'NoMz2onE',
-                          style: TextStyle(fontSize: _height * 0.02),
+                    Expanded(
+                      child: Container(
+                        decoration: new BoxDecoration(
+                          color: Color(0xffF8F8F8),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(_height * 0.01)),
+                        ),
+                        margin: EdgeInsets.only(left: _width * 0.05),
+                        child: Padding(
+                          padding: EdgeInsets.all(_height * 0.01),
+                          child: Text(
+                            'NoMz2onE',
+                            style: TextStyle(fontSize: _height * 0.02),
+                          ),
                         ),
                       ),
                     ),
