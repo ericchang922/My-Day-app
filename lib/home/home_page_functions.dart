13813f034abb6db5033067bc29c60c8e11414515
ChangCheWei <10656004@ntub.edu.tw>
2021-10-16 00:09:53 +0800

    Create function file of schedule table

--- lib/home/home_page_functions.dart
+++ lib/home/home_page_functions.dart
@@ -1,12 +1,12 @@
-timeOfDateTime(DateTime d) {
+Duration timeOfDateTime(DateTime d) {
   return Duration(hours: d.hour, minutes: d.minute);
 }
 
-dateOfDateTime(DateTime d) {
+DateTime dateOfDateTime(DateTime d) {
   return DateTime.utc(d.year, d.month, d.day);
 }
 
-getMon(DateTime today) {
+DateTime getMon(DateTime today) {
   int daysAfter = today.weekday - 1;
   return DateTime.utc(today.year, today.month, today.day - daysAfter);
 }
---
+++ lib/home/home_schedule/schedule_table_function.dart
@@ -0,0 +1,96 @@
+import 'package:My_Day_app/models/schedule/schedule_list_model.dart';
+import 'package:My_Day_app/schedule/schedule_dialog.dart';
+import 'package:flutter/material.dart';
+
+double timeHeight(String start, String end, double fullHeight) {
+  DateTime startTime = DateTime.parse('2000-01-01 ' + start);
+  DateTime endTime = DateTime.parse('2000-01-01 ' + end);
+  double h =
+      endTime.difference(startTime).inMinutes / Duration(hours: 1).inMinutes;
+  if (h < 0) h = h * -1;
+  if (h >= 24) h = 0;
+
+  return h * fullHeight * 0.07;
+}
+
+class Create {
+  BuildContext context;
+  Size _size;
+  double height;
+  double width;
+  Color _topColor = Color(0xffF6E0A4);
+  Color _weekDayColor = Color(0xffF6F6F6);
+  Color _sectionColor = Color(0xffFFFAF0);
+
+  Create({this.context}){
+    _size = MediaQuery.of(context).size;
+    height = _size.height;
+    width = _size.width;
+  }
+
+  static Container _createTableRow(String txt, Color bkcolor, double _height) =>
+      Container(
+        color: bkcolor,
+        height: _height,
+        child: Center(
+          child: Text(
+            txt,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      );
+
+  Container top(String date) => _createTableRow(date, _topColor, height * 0.02);
+
+  Container weekDay(String weekDayTxt) =>
+      _createTableRow(weekDayTxt, _weekDayColor, height * 0.05);
+
+  Container time(String start, String end) => Container(
+        color: _weekDayColor,
+        height: timeHeight(start, end, height),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              start.toString(),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              end.toString(),
+              textAlign: TextAlign.center,
+            )
+          ],
+        )),
+      );
+
+  Container subject(String subjectName, double h) {
+    Container created;
+    if (subjectName == null || subjectName == 'null') {
+      created = _createTableRow('', null, h);
+    } else {
+      created = _createTableRow(subjectName, _sectionColor, h);
+    }
+    return created;
+  }
+
+  List<Widget> dateBtn(List<DateTime> dateList, double _tableContentHeight, ScheduleGetList scheduleList) {
+    List<Widget> dateBtnList = [];
+    for (DateTime d in dateList) {
+      dateBtnList.add(
+        Container(
+          height: _tableContentHeight,
+          width: width / 8,
+          child: TextButton(
+            style: ButtonStyle(
+                padding:
+                    MaterialStateProperty.all<EdgeInsets>(EdgeInsets.zero)),
+            child: Text(''),
+            onPressed: () => scheduleDialog(context, d, scheduleList),
+          ),
+        ),
+      );
+    }
+    return dateBtnList;
+  }
+}
