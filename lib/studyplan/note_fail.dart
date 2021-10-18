b420dbf8008bde9b4f81b0ccfaeca2a60df7cd6f
sharron <10656035@ntub.edu.tw>
2021-10-18 12:52:39 +0800

    Add note_fail

---
+++ lib/studyplan/note_fail.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+Future<bool> notefailDialog(BuildContext context,String alertTitle, String alertTxt) async {
+ double _height = MediaQuery.of(context).size.height;
+  return showDialog<bool>(
+          context: context,
+        builder: (_) => AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(_height * 0.03))),
+          title: Text(alertTitle),
+          content: Text(alertTxt),
+            ),
+      );
+  }
+  
