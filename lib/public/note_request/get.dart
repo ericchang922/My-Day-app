b9a1b1d409f9d654b972621c953a0031cda25546
sharron <10656035@ntub.edu.tw>
2021-10-27 18:36:12 +0800

    Add note_request delete,edit,get

---
+++ lib/public/note_request/get.dart
@@ -0,0 +1,27 @@
+
+import 'package:My_Day_app/models/note/get_note_model.dart';
+import 'package:My_Day_app/public/request.dart';
+import 'package:flutter/material.dart';
+
+class Get {
+  BuildContext context;
+  String uid;
+  int noteNum;
+  Map<String, String> _data;
+  GetNoteModel _response;
+
+  _request() async {
+    Request request = Request();
+    await request.noteGet(context, _data);
+    this._response = await request.getNote();
+  }
+
+  Get({this.uid,this.noteNum}) {
+    _data = {'uid': uid,'noteNum': noteNum.toString()};
+  }
+
+  Future<GetNoteModel> getData() async {
+    await _request();
+    return this._response;
+  }
+}
