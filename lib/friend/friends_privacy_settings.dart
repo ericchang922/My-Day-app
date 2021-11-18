7de9acb65eb6de81677e1edf83b99787cd7e5fe2
sharron <10656035@ntub.edu.tw>
2021-11-19 02:10:57 +0800

    Update friends_privacy_settings

--- lib/friend/friends_privacy_settings.dart
+++ lib/friend/friends_privacy_settings.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 
-const PrimaryColor = const Color(0xFFF86D67);
+// 
 
 class FriendsPrivacySettingsPage extends StatefulWidget {
   @override
@@ -9,210 +10,161 @@ class FriendsPrivacySettingsPage extends StatefulWidget {
     return FriendsPrivacySettings();
   }
 }
-
 class FriendsPrivacySettings extends State {
-  get child => null;
-  get left => null;
-  bool _isCheck;
-  bool _isCheckstroke;
-  bool _isCheckreciprocal;
-  bool _isCheckgroup;
-  @override
-  void initState() {
-    super.initState();
-    _isCheck = false;
-    _isCheckstroke = false;
-    _isCheckreciprocal = false;
-    _isCheckgroup = false;
-  }
-
+  bool switchValue = false;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xffF86D67),
-          title: Text('好友隱私設定', style: TextStyle(fontSize: 20)),
-          leading: IconButton(
-            icon: Icon(Icons.chevron_left),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ),
-        body: ListView(children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(right: 15, left: 35),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text.rich(TextSpan(
-                  children: <InlineSpan>[
-                    WidgetSpan(
-                      child: new Image.asset(
-                        "assets/images/search.png",
-                        width: 20,
-                      ),
-                    ),
-                    TextSpan(text: 'xxxxxx',style: TextStyle(fontSize: 20,)),
-                  ],
-                )),
-                PopupMenuButton<int>(
-                  offset: Offset(0, 50),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  icon: Icon(Icons.more_vert),
-                  itemBuilder: (context) => [
-                    PopupMenuItem<int>(
-                        value: 0,
-                        child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text('玩聚邀請',
-                            style: TextStyle(fontSize: 20,
-                            // alignment: Alignment.center,
-                                ),
-                              ),
-                            Switch(
-                              value: _isCheck,
-                              onChanged: _changed,
-                              activeColor: Colors.white,
-                              activeTrackColor: Color(0xffF86D67),
-                              // inactiveThumbColor: Color(0xffF86D67),
-                              // inactiveTrackColor: Color(0xffF86D67),
-                            ),      
-                  ])),
-                    PopupMenuDivider(
-                      height: 1,
-                    ),
-                    PopupMenuItem<int>(
-                      value: 1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text('公開課表',
-                            style: TextStyle(fontSize: 20,
-                            // alignment: Alignment.center,
-                                ),
-                              ),
-                            Switch(
-                              value: _isCheckstroke,
-                              onChanged: _changedstroke,
-                              activeColor: Colors.white,
-                              activeTrackColor: Color(0xffF86D67),
-                              // inactiveThumbColor: Color(0xffF86D67),
-                              // inactiveTrackColor: Color(0xffF86D67),
-                            ),      
-                  ]))],
-                ),
-              ],
+    return StreamBuilder(
+      stream: block.darkThemeEnabled,
+      initialData: false,
+      builder: (context, snapshot) {
+        switchValue = snapshot.data;
+        return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            title: Text('好友隱私設定', style: TextStyle(fontSize: 20)),
+            leading: IconButton(
+              icon: Icon(Icons.chevron_left),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
           ),
+          body: ListView(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(right: 15, left: 35),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text.rich(TextSpan(
+                      children: <InlineSpan>[
+                        WidgetSpan(
+                          child: new Image.asset(
+                            "assets/images/search.png",
+                            width: 20,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'xxxxxx',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  PopupMenuButton<int>(
+                    offset: Offset(0, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                    icon: Icon(Icons.more_vert),
+                    itemBuilder: (context) => [
+                      PopupMenuItem<int>(
+                        value: 1,
+                        child: ListTile(
+                          title: Text(
+                                  '玩聚邀請',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                          ),
+                          trailing: StatefulBuilder(builder:
+                            (BuildContext context, StateSetter setState) {
+                              return Switch(        
+                                value: switchValue,
+                                onChanged: (newValue) {
+                                  block.changeTheme1(newValue);
+                                  print(switchValue);
+                                  setState(() {});
+                                },
+                                activeColor: Colors.white,
+                                activeTrackColor: Color(0xffF86D67),
+                                // inactiveThumbColor: Color(0xffF86D67),
+                                // inactiveTrackColor: Color(0xffF86D67),
+                            );}
+                          ),
+                        ),
+                      ),
+                      PopupMenuDivider(
+                        height: 1,
+                      ),
+                      PopupMenuItem<int>(
+                        value: 1,
+                        child: ListTile(
+                          title: Text(
+                                  '公開課表',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                          ),
+                          trailing: StatefulBuilder(builder:
+                            (BuildContext context, StateSetter setState) {
+                              return Switch(        
+                                value: switchValue,
+                                onChanged: (newValue) {
+                                  block.changeTheme1(newValue);
+                                  print(switchValue);
+                                  setState(() {});
+                                },
+                                activeColor: Colors.white,
+                                activeTrackColor: Color(0xffF86D67),
+                                // inactiveThumbColor: Color(0xffF86D67),
+                                // inactiveTrackColor: Color(0xffF86D67),
+                            );}
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ]),
+          ),
+            
+             
+          
           Container(
             margin: EdgeInsets.only(top: 10),
             color: Color(0xffE3E3E3),
             constraints: BoxConstraints.expand(height: 1.0),
           ),
-          Container(
-            margin: EdgeInsets.only(right: 15, left: 35),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text.rich(TextSpan(
-                  children: <InlineSpan>[
-                    WidgetSpan(
-                      child: new Image.asset(
-                        "assets/images/search.png",
-                        width: 20,
-                      ),
-                    ),
-                    TextSpan(text: 'xxxxxx',style: TextStyle(fontSize: 20,)),
-                  ],
-                )),
-                PopupMenuButton<int>(
-                  offset: Offset(0, 50),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  icon: Icon(Icons.more_vert),
-                  itemBuilder: (context) => [
-                    PopupMenuItem<int>(
-                        value: 0,
-                        child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text('玩聚邀請',
-                            style: TextStyle(fontSize: 20,
-                            // alignment: Alignment.center,
-                                ),
-                              ),
-                            Switch(
-                              value: _isCheckreciprocal,
-                              onChanged: _changedreciprocal,
-                              activeColor: Colors.white,
-                              activeTrackColor: Color(0xffF86D67),
-                              // inactiveThumbColor: Color(0xffF86D67),
-                              // inactiveTrackColor: Color(0xffF86D67),
-                            ),      
-                  ])),
-                    PopupMenuDivider(
-                      height: 1,
-                    ),
-                    PopupMenuItem<int>(
-                      value: 1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text('公開課表',
-                            style: TextStyle(fontSize: 20,
-                            // alignment: Alignment.center,
-                                ),
-                              ),
-                            Switch(
-                              value: _isCheckgroup,
-                              onChanged: _changedgroup,
-                              activeColor: Colors.white,
-                              activeTrackColor: Color(0xffF86D67),
-                              // inactiveThumbColor: Color(0xffF86D67),
-                              // inactiveTrackColor: Color(0xffF86D67),
-                            ),      
-                  ]))],
-                ),
-              ],
-            ),
+              // actions: <Widget>[
+              //   PopupMenuButton(itemBuilder: (context) {
+              //     return [
+              //       PopupMenuItem(
+              //           child: ListTile(
+              //         title: Text("Dark Theme"),
+              //         trailing: StatefulBuilder(builder:
+              //             (BuildContext context, StateSetter setState) {
+              //           return Switch(
+              //             value: switchValue,
+              //             onChanged: (newValue) {
+              //               block.changeTheme1(newValue);
+              //               print(switchValue);
+              //               setState(() {});
+              //             },
+              //           );
+              //         }),
+              //       )), //Problem
+              //     ];
+              //   })
+              // ],
+            ]),
           ),
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            color: Color(0xffE3E3E3),
-            constraints: BoxConstraints.expand(height: 1.0),
-          ),
-        ],
-      ),
-    
-    ));
-  }
-
-  void _changed(isCheck) {
-    setState(() {
-      _isCheck = isCheck;
-    });
-  }
-
-  void _changedstroke(isCheck) {
-    setState(() {
-      _isCheckstroke = isCheck;
-    });
-  }
-
-  void _changedreciprocal(isCheck) {
-    setState(() {
-      _isCheckreciprocal = isCheck;
-    });
-  }
-
-  void _changedgroup(isCheck) {
-    setState(() {
-      _isCheckgroup = isCheck;
-    });
+        );
+      },
+    );
   }
 }
+
+class Block {
+  final _themeContol = StreamController<bool>();
+
+  void changeTheme1(bool value) {
+    _themeContol.sink.add(value);
+  }
+
+  get darkThemeEnabled => _themeContol.stream;
+}
+
+final block = Block();
