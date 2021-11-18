c73b8d89615e6b793c65493c497b0c43dce6ad4f
sharron <10656035@ntub.edu.tw>
2021-11-19 02:22:39 +0800

    add notice_temporary

---
+++ lib/public/setting_request/notice_temporary .dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:My_Day_app/public/request.dart';
+
+class NoticeTemporary {
+  BuildContext context;
+  String uid;
+  bool isTemporary	;
+  Map<String, dynamic> data;
+
+  bool _isError;
+
+  _request() async {
+    Request request = Request();
+    await request.noticetemporary(context, data);
+    this._isError = await request.getIsError();
+  }
+
+  NoticeTemporary({this.context, this.uid, this.isTemporary	}) {
+    data = {'uid': uid, 'isTemporary	': isTemporary	};
+  }
+  getIsError() async {
+    await _request();
+    return this._isError;
+  }
+}
