b06b3c88d99647de46cbcf94935de257b6dd0e29
sharron <10656035@ntub.edu.tw>
2021-11-19 02:12:52 +0800

    Update theme_model

--- lib/models/setting/theme_model.dart
+++ lib/models/setting/theme_model.dart
@@ -1,22 +1,22 @@
 import 'dart:convert';
 
-ThemeModel themeModelFromJson(String str) => ThemeModel.fromJson(json.decode(str));
+GetThemesModel getThemesModelFromJson(String str) => GetThemesModel.fromJson(json.decode(str));
 
-String themeModelToJson(ThemeModel data) => json.encode(data.toJson());
+String getThemesModelToJson(GetThemesModel data) => json.encode(data.toJson());
 
-class ThemeModel {
-    ThemeModel({
-        this.colorCode		,
+class GetThemesModel {
+    GetThemesModel({
+        this.themeId			,
     });
     
-    String colorCode;
+    int themeId	;
 
-    factory ThemeModel.fromJson(Map<String, dynamic> json) => ThemeModel(
-      colorCode: json["colorCode"],
+    factory GetThemesModel.fromJson(Map<String, dynamic> json) => GetThemesModel(
+      themeId	: json["themeId	"],
     );
 
     Map<String, dynamic> toJson() => {
-        "colorCode": colorCode,
+        "themeId	": themeId	,
     };
 }
 
