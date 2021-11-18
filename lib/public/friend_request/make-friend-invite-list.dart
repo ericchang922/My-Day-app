d3490dd6670f3500df2d6c564168da5a6cd0eb49
sharron <10656035@ntub.edu.tw>
2021-11-19 02:24:37 +0800

    add make-friend-invite-list

---
+++ lib/public/friend_request/make-friend-invite-list.dart
@@ -0,0 +1,25 @@
+import 'package:My_Day_app/models/friend/make-friend-invite-list_model.dart';
+import 'package:flutter/material.dart';
+import 'package:My_Day_app/public/request.dart';
+
+class MakeFriendInviteList {
+  BuildContext context;
+  String uid;
+  Map<String, String> data;
+  MakeFriendInviteListModel _response;
+
+  _request() async {
+    Request request = Request();
+    await request.makefriendinviteList(context, data);
+    _response = await request.getMakeFriendInviteListGet();
+  }
+
+  MakeFriendInviteList({this.uid}) {
+    data = {'uid': uid};
+  }
+
+  getData() async {
+    await _request();
+    return this._response;
+  }
+}
