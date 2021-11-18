3248ddbcaa5c3f815f3b5c3877321f710031ec19
sharron <10656035@ntub.edu.tw>
2021-11-19 02:21:54 +0800

    Update privacy_timetable

---
+++ lib/public/setting_request/privacy_timetable.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:My_Day_app/public/request.dart';
+
+class PrivacyTimetable {
+  BuildContext context;
+  String uid;
+  bool isPublic;
+  Map<String, dynamic> data;
+
+  bool _isError;
+
+  _request() async {
+    Request request = Request();
+    await request.privacytimetable(context, data);
+    this._isError = await request.getIsError();
+  }
+
+  PrivacyTimetable({this.context, this.uid, this.isPublic }) {
+    data = {'uid': uid, 'isPublic': isPublic};
+  }
+  getIsError() async {
+    await _request();
+    return this._isError;
+  }
+}
