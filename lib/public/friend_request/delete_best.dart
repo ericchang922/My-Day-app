0d9c0bd01b4f874887c42fae50991a10be8952be
sharron <10656035@ntub.edu.tw>
2021-11-19 02:25:00 +0800

    Add delete_best

---
+++ lib/public/friend_request/delete_best.dart
@@ -0,0 +1,29 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:My_Day_app/public/request.dart';
+
+class DeleteBestFriend {
+  BuildContext context;
+  String uid;
+  String friendId;
+  
+  Map<String, dynamic> data;
+
+  bool _isError;//註冊結果
+
+  _request() async {
+    Request request = Request();
+    await request.bestfrienddelete(context, data);
+    this._isError = await request.getIsError();
+  }
+
+  DeleteBestFriend({this.context, this.uid, this.friendId }) {
+    data = {'uid': uid, 'friendId': friendId };
+  }
+  getIsError() async {
+    await _request();
+    return this._isError;
+  }
+}
+
