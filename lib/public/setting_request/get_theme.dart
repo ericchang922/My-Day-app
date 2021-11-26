501423728f0d47385e0447303e13e327d47faf0c
sharron <10656035@ntub.edu.tw>
2021-11-26 10:03:12 +0800

    Update get_theme,notice setting_request

--- lib/public/setting_request/get_theme.dart
+++ lib/public/setting_request/get_theme.dart
@@ -1,27 +1,25 @@
-// import 'package:My_Day_app/models/setting/theme_model.dart';
-// import 'package:flutter/material.dart';
+import 'package:My_Day_app/models/setting/themes_model.dart';
+import 'package:flutter/material.dart';
+import 'package:My_Day_app/public/request.dart';
 
+class GetThemes {
+  BuildContext context;
+  String uid;
+  Map<String, String> data;
+  GetThemesModel _response;
 
-// import 'package:My_Day_app/public/request.dart';
+  _request() async {
+    Request request = Request();
+    await request.getthemes(context, data);
+    _response = await request.getThemesGet();
+  }
 
-// class GetThemes {
-//   BuildContext context;
-//   String uid;
-//   Map<String, String> data;
-//   GetThemesModel _response;
+  GetThemes({this.uid}) {
+    data = {'uid': uid};
+  }
 
-//   _request() async {
-//     Request request = Request();
-//     await request.getthemes(context, data);
-//     _response = await request.getThemesGet();
-//   }
-
-//   GetThemes({this.uid}) {
-//     data = {'uid': uid};
-//   }
-
-//   getData() async {
-//     await _request();
-//     return this._response;
-//   }
-// }
+  getData() async {
+    await _request();
+    return this._response;
+  }
+}
