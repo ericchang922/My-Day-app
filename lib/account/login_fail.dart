129bcc626db426d466008d8d439b2f9c2efeec76
chehui0512 <10656009@ntub.edu.tw>
2021-11-28 19:23:16 +0800

    Update safeArea and Delete remark

--- lib/account/login_fail.dart
+++ lib/account/login_fail.dart
@@ -1,68 +1,61 @@
-
 import 'package:flutter/material.dart';
 
-Future<bool> loginfailDialog(BuildContext context,String alertTitle, String alertTxt) async {
- double _height = MediaQuery.of(context).size.height;
-
-
-  return showDialog<bool>(
-          context: context,
-        builder: (_) => AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(_height * 0.03))),
-          title: Text(alertTitle),
-          content: Text(alertTxt),
-                
-              
-            ),
-      );
-  }
-Future<bool> forgetfailDialog(BuildContext context,String alertTitle, String alertTxt) async {
- double _height = MediaQuery.of(context).size.height;
-
+Future<bool> loginfailDialog(
+    BuildContext context, String alertTitle, String alertTxt) async {
+  double _height = MediaQuery.of(context).size.height;
 
   return showDialog<bool>(
-          context: context,
-        builder: (_) => AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(_height * 0.03))),
-          title: Text(alertTitle),
-          content: Text(alertTxt),
-                
-              
-            ),
-      );
-  }
-Future<bool> changefailDialog(BuildContext context,String alertTitle, String alertTxt) async {
- double _height = MediaQuery.of(context).size.height;
+    context: context,
+    builder: (_) => AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(_height * 0.03))),
+      title: Text(alertTitle),
+      content: Text(alertTxt),
+    ),
+  );
+}
 
-
-  return showDialog<bool>(
-          context: context,
-        builder: (_) => AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(_height * 0.03))),
-          title: Text(alertTitle),
-          content: Text(alertTxt),
-                
-              
-            ),
-      );
-  }
-  Future<bool> codefailDialog(BuildContext context,String alertTitle, String alertTxt) async {
- double _height = MediaQuery.of(context).size.height;
-
+Future<bool> forgetfailDialog(
+    BuildContext context, String alertTitle, String alertTxt) async {
+  double _height = MediaQuery.of(context).size.height;
 
   return showDialog<bool>(
-          context: context,
-        builder: (_) => AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(_height * 0.03))),
-          title: Text(alertTitle),
-          content: Text(alertTxt),
-                
-              
-            ),
-      );
-  }
+    context: context,
+    builder: (_) => AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(_height * 0.03))),
+      title: Text(alertTitle),
+      content: Text(alertTxt),
+    ),
+  );
+}
 
+Future<bool> changefailDialog(
+    BuildContext context, String alertTitle, String alertTxt) async {
+  double _height = MediaQuery.of(context).size.height;
+
+  return showDialog<bool>(
+    context: context,
+    builder: (_) => AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(_height * 0.03))),
+      title: Text(alertTitle),
+      content: Text(alertTxt),
+    ),
+  );
+}
+
+Future<bool> codefailDialog(
+    BuildContext context, String alertTitle, String alertTxt) async {
+  double _height = MediaQuery.of(context).size.height;
+
+  return showDialog<bool>(
+    context: context,
+    builder: (_) => AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(_height * 0.03))),
+      title: Text(alertTitle),
+      content: Text(alertTxt),
+    ),
+  );
+}
