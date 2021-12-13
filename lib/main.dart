b197a6ed0426218137ad794a656f9f999c3a339b
ChangCheWei <10656004@ntub.edu.tw>
2021-12-13 19:49:24 +0800

    Fix create schedule after login can select weeks and days

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:My_Day_app/public/account_request/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -17,10 +18,15 @@ Future<void> main() async {
   print('main -- login uid: $id');
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  runApp(id == null ? MyApp() : HomeWidget());
+  runApp(MyApp(id));
 }
 
 class MyApp extends StatelessWidget {
+  String uid;
+  Widget page;
+  MyApp(this.uid) {
+    uid == null ? page = LoginPage() : page = HomeUpdate(child: Home());
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,25 +36,16 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate
       ],
+      theme: ThemeData(
+        primaryColor: Color(0xffF86D67), //主色
+        primaryColorLight: Color(0xffFFAAA6), //次色
+        primaryColorDark: Color(0xffFFF5F5), //超淡色
+        accentColor: Color(0xffFFB5B5), //淡色
+        bottomAppBarColor: Color(0xffFB8B86),
+      ),
       supportedLocales: [const Locale('zh', 'TW')],
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: page,
     );
-  }
-}
-
-class HomeWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Color(0xffF86D67), //主色
-          primaryColorLight: Color(0xffFFAAA6), //次色
-          primaryColorDark: Color(0xffFFF5F5), //超淡色
-          accentColor: Color(0xffFFB5B5), //淡色
-          bottomAppBarColor: Color(0xffFB8B86),
-        ),
-        home: HomeUpdate(child: Home()));
   }
 }
